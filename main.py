0b21c04a0ed60148d7afd660b55393497a01846c
talkingFox <futureinrevolution@gmail.com>
2021-01-14 00:19:50 +0300

    all is work

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import sys
 from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from hierarchy_keeper import HierarchyKeeper
 from move_widget import MoveWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        hierarchy_keeper = HierarchyKeeper(self)
-        self.setCentralWidget(hierarchy_keeper.get_root_move_widget())
+        self.setCentralWidget(MoveWidget("content"))
 
     def keyPressEvent(self, a0: QKeyEvent):
         self.centralWidget().key_press(a0.key())
--- move_widget.py
+++ move_widget.py
@@ -1,38 +1,49 @@
 from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel
-
+import os
 
 class MoveWidget(QWidget):
-    def __init__(self):
+    def __init__(self, directory):
         super().__init__()
-
+        self.current_directory = directory
+        self.update_move_sections()
         self.ESCAPE_KEY = 16777216
 
+        self.label = QLabel()
+        self.label.setWordWrap(True)
         self.lt = QHBoxLayout()
+        self.lt.addWidget(self.label)
+        self.update_label_text()
         self.setLayout(self.lt)
 
+    def update_move_sections(self):
+        if os.path.isdir(self.current_directory):
+            self.move_sections = os.listdir(self.current_directory)
+        else:
+            self.move_sections = []
+    def update_label_text(self):
+        if os.path.isdir(self.current_directory):
+            self.label.setText(
+                "_".join(self.move_sections)
+            )
+        else:
+            with open(self.current_directory, "r", encoding="utf-8") as f:
+                self.label.setText(f.read())
     def init_label(self):
         self.lt.addWidget(QLabel(self.data))
 
 
 
     def key_press(self, key_id):
-        if key_id == self.ESCAPE_KEY:
-            self.main_window.setCentralWidget(
-                self.previous_widget_class
-                (
-                    self.data_provider,
-                    self.main_window,
-                    self.states_provider
-                )
-            )
-            return
         index = key_id - 49
-        if 0 <= index < len(self.move_list):
-            self.main_window.setCentralWidget(self.move_list[index](
-                self.data_provider,
-                self.main_window,
-                self.states_provider
-            ))
+        if key_id == self.ESCAPE_KEY:
+            if self.current_directory == "content":
+                return
+            self.current_directory = "/".join(
+                self.current_directory.split("/")[:-1]
+            )
         else:
-            return
+            self.current_directory += "/" + self.move_sections[index]
+
+        self.update_move_sections()
+        self.update_label_text()
