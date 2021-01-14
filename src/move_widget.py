51abbae13cc7288166194e5cfbf1255205f5e457
TalkingFox <futureinrevolution@gmail.com>
2021-01-14 07:11:09 +0500

    string_formating logic was sharded

--- src/move_widget.py
+++ src/move_widget.py
@@ -5,6 +5,7 @@ from config import SLASH_REPLACER, WND_HEIGHT, WND_WIDTH
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QHBoxLayout, QWidget
 
+from src.new_label_text_provider import NewLabelTextProvider
 from src.scrollable_label import ScrollableLabel
 
 
@@ -14,7 +15,7 @@ class MoveWidget(QWidget):
         self.current_directory = directory
         self.update_move_sections()
         self.ESCAPE_KEY = 16777216
-
+        self.new_label_text_provider = NewLabelTextProvider()
         self.label = ScrollableLabel()
         self.lt = QHBoxLayout()
         self.lt.addWidget(self.label)
@@ -25,9 +26,6 @@ class MoveWidget(QWidget):
         self.init_font()
         self.init_palette()
 
-    @staticmethod
-    def get_normalized_labels(labels: List[str]):
-        return [label.replace(SLASH_REPLACER, "/") for label in labels]
 
     def init_font(self):
         font = self.font()
@@ -45,14 +43,13 @@ class MoveWidget(QWidget):
         else:
             self.move_sections = []
 
-    def update_label_text(self):
-        if os.path.isdir(self.current_directory):
-            labels = self.get_normalized_labels(self.move_sections)
-            self.label.setText("\n".join(labels))
-        else:
-            with open(self.current_directory, "r", encoding="utf-8") as doc:
-                self.label.setText(doc.read())
 
+    def update_label_text(self):
+        self.new_label_text_provider.update_label_text(
+            self.current_directory,
+            self.label,
+            self.move_sections
+        )
     def key_press(self, key_id):
         index = key_id - 49
         if key_id == self.ESCAPE_KEY:
